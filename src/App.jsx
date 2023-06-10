ab07f345f00e4ccdb418cb8507f8e7bd7ed2121b
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-06-10 16:25:00 -0700

    Add gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div id="wrap">
-      <div className="bg-purple grid-rows-layout grid min-h-screen b">
-        <header className="row-start-1 row-end-2 top-0 w-full min-h-[80px]">
-          <h1>this works</h1>
+      <div className="bg-purple grid-rows-layout grid min-h-screen bg-gradient-to-br from-[#000] via-[#45248d] via-60% to-[#200871]">
+        <header className="bg-black row-start-1 row-end-2 top-0 w-full min-h-[80px]">
+          <nav id="nav" className=""></nav>
         </header>
       </div>
     </div>
